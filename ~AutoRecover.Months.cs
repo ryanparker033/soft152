568c951e1d23a6e2a998c060ce46f2508e34eb0d
ryanparker033 <43572925+ryanparker033@users.noreply.github.com>
2018-11-22 11:46:50 +0000

    classes of months and years

---
+++ ~AutoRecover.Months.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MET_Office
+{
+    class Months
+    {
+        //Varibles 
+        private int monthID;
+        private int maximumtemp;
+        private int mimimumtemp;
+        private int airfrost;
+        private int millimetersrain;
+        private int hourssunshine;
+        //constructor 
+        public Months(int themonthID, int themaximumtemp, int themimimumtemp,
+                      int theairfrost, int themillmetersofrain,
+                      int thehoursofsunshine)
+        {
+            monthID = themonthID;
+            maximumtemp = themaximumtemp;
+            mimimumtemp = themimimumtemp;
+            airfrost = theairfrost;
+            millimetersrain = themillmetersofrain;
+            hourssunshine = thehoursofsunshine;
+        }
+        //Setters
+        public void setmonth(int inmonth)
+        {
+            monthID = inmonth;
+        }
+
+
+        public void setmaxtemp(int inmaxtemp)
+        {
+         maximumtemp = inmaxtemp;
+        }
+
+        public void setmintemp(int inmintemp)
+        {
+            mimimumtemp = inmintemp;
+        }
+                
+        public void numofdayairfrost(int inairfrost)
+        {
+            airfrost = inairfrost;
+        }
+       public void millimetersofrain(int inmillimetersofrain)
+       {
+         millimetersrain = inmillimetersofrain;
+        }
+        public void hoursofsunshine(int inhoursofsunshine)
+        {
+            hourssunshine = inhoursofsunshine;
+        }
+
+        //getters
+
+        public string getmonth()
+        {
+            return monthID.ToString();
+        }
+
+        public string getmaxtemp()
+        {
+            return maximumtemp.ToString();
+        }
+
+        public string getmaxtemperture()
+        {
+            return maximumtemp.ToString();
+        }
+
+        public string getmintemp()
+        {
+            return mimimumtemp.ToString();
+        }
+
+        public string getairfrost()
+        {
+            return airfrost.ToString();
+        }
+
+        public string getmillimetersofrain()
+        {
+            return millimetersrain.ToString();
+        }
+
+        public string gethoursofsunshine()
+        {
+            return hourssunshine.ToString();
+        }
+    }
+
+}
---
+++ ~AutoRecover.Years.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MET_Office
+{
+    class Years
+    {
+        //varibles for the year class
+        private int yearID;
+        private int yeardescription;
+        private int allmonths;
+        Years[] months;
+
+        //two constuctors for the year class
+        public Years(int theyearID, int theyeardescripion, int theallmonths)
+        {
+            yearID = theyearID;
+            yeardescription = theyeardescripion;
+            allmonths = theallmonths;
+            months = null;
+        }
+
+        public Years(int theyearID, int theyearsecription, int theallmonths, Years[] themonths)
+        {
+            yearID = theyearID;
+            yeardescription = theyearsecription;
+            allmonths = theallmonths;
+            months = themonths;
+        }
+        //get the details for each of the years
+        public string getDetails()
+        {
+            string output;
+            output = String.Format("Year is {0} which was {1} and had {2}",
+                     yearID, yeardescription, months.Length);
+            return output;
+        }
+        //getter
+        public Years[] getallmonths()
+        {
+            return months;
+        }
+        //setter
+        public void setallmonths(Years[] themonths)
+        {
+            months = themonths;
+        }
+    }
+            
+    
+}
